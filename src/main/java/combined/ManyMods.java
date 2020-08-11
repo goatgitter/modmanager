eb9971d9a78471222fa24fb58c4107ac7da2022e
h1ppyChick <69547279+h1ppyChick@users.noreply.github.com>
2020-08-11 19:00:26 -0400

    Add files via upload
    Original Version of the ManyMods source code

---
+++ src/main/java/combined/ManyMods.java
@@ -0,0 +1,30 @@
+package combined;
+
+import net.fabricmc.api.ModInitializer;
+/**
+ * 
+ * @author H1ppyChick & Torphedo 
+ * @version 1.0.0 
+ * @since 08/11/2020
+ * 
+ * This is Minecraft Fabric mod which combines the following original mods into a single JAR :
+ * appleskin : https://www.curseforge.com/minecraft/mc-mods/appleskin
+ * modmenu : https://www.curseforge.com/minecraft/mc-mods/modmenu
+ * right-click-clear : https://www.curseforge.com/minecraft/mc-mods/right-click-clear
+ * CleanView : https://www.curseforge.com/minecraft/mc-mods/clear-view
+ * clear-skies : https://www.curseforge.com/minecraft/mc-mods/clear-skies
+ * connected-block-textures : https://www.curseforge.com/minecraft/mc-mods/connected-block-textures
+ * experimentaway : https://www.curseforge.com/minecraft/mc-mods/experimentaway
+ * fastbench : https://www.curseforge.com/minecraft/mc-mods/fastbench-for-fabric
+ * flighthud : https://www.curseforge.com/minecraft/mc-mods/flighthud
+ * horse-stats : https://www.curseforge.com/minecraft/mc-mods/horsestatsvanilla
+ * MissingBits : https://www.curseforge.com/minecraft/mc-mods/missingbits
+ * nomoreglowingpots : https://www.curseforge.com/minecraft/mc-mods/nomoreglowingpots
+ * notenoughcrashes : https://www.curseforge.com/minecraft/mc-mods/not-enough-crashes
+ */
+public class ManyMods implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// Do Nothing
+	}
+}
