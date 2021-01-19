6d9d8ec13603e21cdabfa8671a7f33f0086dbd25
h1ppyChick <69547279+h1ppyChick@users.noreply.github.com>
2021-01-19 12:21:46 -0500

    Remove unused Import.

--- src/main/java/com/github/h1ppyChick/modmenuext/gui/ChildModsScreen.java
+++ src/main/java/com/github/h1ppyChick/modmenuext/gui/ChildModsScreen.java
@@ -13,7 +13,6 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 
