b08f7724b451b9b21eecd872c96db6e772d56d3c
h1ppyChick <69547279+h1ppyChick@users.noreply.github.com>
2021-01-20 14:19:13 -0500

    Refactoring and hide file name extension.

--- src/main/java/com/github/h1ppyChick/modmenuext/gui/ChildModsScreen.java
+++ src/main/java/com/github/h1ppyChick/modmenuext/gui/ChildModsScreen.java
@@ -26,14 +26,18 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 
 public class ChildModsScreen extends TwoListsWidgetScreen{
-	// Constants
+	/***************************************************
+	 *              CONSTANTS
+	 **************************************************/
 	private final static String TITLE_ID = ModMenuExt.MOD_ID + ".config.screen.title";
 	private static final Identifier ADD_BUTTON_LOCATION = new Identifier(ModMenuExt.MOD_ID, "add.png");
 	private static final TranslatableText TEXT_ADD_TOOLTIP = new TranslatableText(ModMenuExt.MOD_ID + ".add.tooltip");
 	private static final TranslatableText TEXT_ADD_DESC = new TranslatableText(ModMenuExt.MOD_ID + ".add.description");
 	private static final TranslatableText TEXT_MOD_LIST = new TranslatableText(ModMenuExt.MOD_ID + ".modlist");
 	
-	// Instance Variables
+	/***************************************************
+	 *              INSTANCE VARIABLES
+	 **************************************************/
 	private Log LOG = new Log("ChildModsScreen");
 	private CombinedLoader cl = new CombinedLoader();
 	private TextFieldWidget listNameInput;
@@ -43,30 +47,23 @@ public class ChildModsScreen extends TwoListsWidgetScreen{
 	private int topRowHeight = 21;
 	private int leftPaneX = 5;
 	
-	
-	// Constructors
+	/***************************************************
+	 *              CONSTRUCTORS
+	 **************************************************/
 	public ChildModsScreen(Screen previousScreen) {
 		super(previousScreen, TITLE_ID);
 	}
 	
-	// Methods
+	/***************************************************
+	 *              METHODS
+	 **************************************************/
 	@Override
 	public void init() {
 		LOG.enter("init");
 		addItemsToScreen();
 		LOG.exit("init");
 	}
-	
-	private void doneButtonClick()
-	{
-		this.onClose();
-	}
-	
-	private void addButtonClick()
-	{
-		SystemToast.add(client.getToastManager(), SystemToast.Type.TUTORIAL_HINT, ModMenuExt.TEXT_WARNING, ModMenuExt.TEXT_NOT_IMPL);
-	}
-	
+
 	private void addItemsToScreen()
 	{
 		paneY = 28;
@@ -96,52 +93,6 @@ public class ChildModsScreen extends TwoListsWidgetScreen{
 		drawListNameInput();
 	}
 	
-	private void loadMod(ChildModEntry mod) {
-		restartRequired = true;
-		ModConfig.requestLoad(mod);
-	}
-	
-	private void unloadMod(ChildModEntry mod) {
-		restartRequired = true;
-		ModConfig.requestUnload(mod);
-	}
-	
-	private void drawDoneButton()
-	{
-		this.addButton(new ButtonWidget(this.width / 3 + 4, this.height - 28, 150, 20, ScreenTexts.DONE, button -> doneButtonClick()));
-	}
-
-	private void drawAddButton()
-	{
-		int addBtnX =  listNameInputX + listNameInputWidth + 5;
-		ButtonWidget addBtn = new ModMenuTexturedButtonWidget(addBtnX, getTopRowY(), topRowHeight, topRowItemHeight, 0, 0, ADD_BUTTON_LOCATION, topRowHeight, 42, button -> {
-			addButtonClick();
-		},TEXT_ADD_TOOLTIP, (buttonWidget, matrices, mouseX, mouseY) -> {
-			ModMenuTexturedButtonWidget button = (ModMenuTexturedButtonWidget) buttonWidget;
-			if (button.isJustHovered()) {
-				this.renderTooltip(matrices, TEXT_ADD_TOOLTIP, mouseX, mouseY);
-			} else if (button.isFocusedButNotHovered()) {
-				this.renderTooltip(matrices, TEXT_ADD_TOOLTIP, button.x, button.y);
-			}
-		}) {
-		};
-		this.addButton(addBtn);
-	}
-	private void drawListNameInput()
-	{
-		String listName = cl.getLoadFile().getFileName().toString();
-		listName = listName.substring(0, listName.lastIndexOf(".txt"));
-		Text listNameText = new LiteralText(listName);
-		this.listNameInput = new TextFieldWidget(this.textRenderer, listNameInputX, getTopRowY(), listNameInputWidth, topRowItemHeight, this.listNameInput, listNameText);
-		this.listNameInput.setText(listName);
-		this.children.add(this.listNameInput);
-		this.setInitialFocus(this.listNameInput);
-	}
-	
-	private int getTopRowY()
-	{
-		return paneY-10;
-	}
 	@Override
 	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
 		this.renderBackground(matrices);
@@ -160,16 +111,6 @@ public class ChildModsScreen extends TwoListsWidgetScreen{
 	@Override
 	public void filesDragged(List<Path> paths) {
 		restartRequired = true;
-		addMods(paths);
-	}
-	
-	public String getListNameInput() {
-		return listNameInput.getText();
-	}
-	
-	// Used with files are dropped into the window
-	private void addMods(List<Path> paths)
-	{
 		Path modsDirectory = cl.getModsDir();
 		
 		List<Path> mods = paths.stream()
@@ -209,6 +150,106 @@ public class ChildModsScreen extends TwoListsWidgetScreen{
 			}
 			this.client.openScreen(this);
 		}, new TranslatableText("modmenu.dropConfirm"), new LiteralText(modList)));
+	}
+	
+	private void loadMod(ChildModEntry mod) {
+		restartRequired = true;
+		ModConfig.requestLoad(mod);
+	}
+	
+	private void unloadMod(ChildModEntry mod) {
+		restartRequired = true;
+		ModConfig.requestUnload(mod);
+	}
+	
+	private int getTopRowY()
+	{
+		return paneY-10;
+	}
+	
+	/***************************************************
+	 *              DONE BUTTON
+	 **************************************************/
+	/**
+	 * Draws the Done Button at the correct position on the screen.
+	 */
+	private void drawDoneButton()
+	{
+		this.addButton(new ButtonWidget(this.width / 3 + 4, this.height - 28, 150, 20, ScreenTexts.DONE, button -> doneButtonClick()));
+	}
+	/**
+	 * Performs the Done Action when the button is clicked.
+	 * Closes the screen, with restart option, if required.
+	 * {@link #onClose()}
+	 */
+	private void doneButtonClick()
+	{
+		this.onClose();
+	}
+	/***************************************************
+	 *              ADD BUTTON
+	 **************************************************/
+	/**
+	 * Draws the Add Button at the correct position on the screen.
+	 */
+	private void drawAddButton()
+	{
+		int addBtnX =  listNameInputX + listNameInputWidth + 5;
+		ButtonWidget addBtn = new ModMenuTexturedButtonWidget(addBtnX, getTopRowY(), topRowHeight, topRowItemHeight, 0, 0, ADD_BUTTON_LOCATION, topRowHeight, 42, button -> {
+			addButtonClick();
+		},TEXT_ADD_TOOLTIP, (buttonWidget, matrices, mouseX, mouseY) -> {
+			ModMenuTexturedButtonWidget button = (ModMenuTexturedButtonWidget) buttonWidget;
+			if (button.isJustHovered()) {
+				this.renderTooltip(matrices, TEXT_ADD_TOOLTIP, mouseX, mouseY);
+			} else if (button.isFocusedButNotHovered()) {
+				this.renderTooltip(matrices, TEXT_ADD_TOOLTIP, button.x, button.y);
+			}
+		}) {
+		};
+		this.addButton(addBtn);
+	}
+	
+	/**
+	 * Performs the Add Action when the button is clicked.
+	 * Implementation is TBD, so just show a user message for now.
+	 */
+	private void addButtonClick()
+	{
+		SystemToast.add(client.getToastManager(), SystemToast.Type.TUTORIAL_HINT, ModMenuExt.TEXT_WARNING, ModMenuExt.TEXT_NOT_IMPL);
+	}
+	/***************************************************
+	 *              LIST NAME INPUT BOX 
+	 **************************************************/
+	
+	/**
+	 * Draws the List Name Input Box at the correct position on the screen.
+	 */
+	private void drawListNameInput()
+	{
+		String listName = cl.getLoadFile().getFileName().toString();
+		listName = listName.substring(0, listName.lastIndexOf(".txt"));
+		Text listNameText = new LiteralText(listName);
+		this.listNameInput = new TextFieldWidget(this.textRenderer, listNameInputX, getTopRowY(), listNameInputWidth, topRowItemHeight, this.listNameInput, listNameText);
+		this.listNameInput.setText(listName);
+		this.children.add(this.listNameInput);
+		this.setInitialFocus(this.listNameInput);
+	}
+	
+	/**
+	 * Makes the cursor blink inside the list name input
+	 */
+	@Override
+	public void tick() {
+		this.listNameInput.tick();
+	}
+	
+	/**
+	 *   Method for retrieving the list name.
+	 *   Will be used later.
+	 * @return String - The List Name from the UI Input.
+	 */
+	public String getListNameInput() {
+		return listNameInput.getText();
 	}
 
 }
