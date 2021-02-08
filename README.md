d51e99e4f07f35f4289b9d75762afa3845c0ebef
goatgitter <69547279+h1ppyChick@users.noreply.github.com>
2021-02-08 14:31:30 -0500

    Fix problem with icon not displaying when expanded.

--- README.md
+++ README.md
@@ -9,9 +9,6 @@ This is Minecraft Fabric mod which extends the Mod Menu mod to add ability to:
 
 * modmenu : https://www.curseforge.com/minecraft/mc-mods/modmenu : MIT License
 
-## Known Issues
-* Mod Icon changes to blank white square after it is expanded.  Displayed correctly after config screen is opened.
-
 ## License
 
 This project is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
--- src/main/java/com/github/goatgitter/modmanager/mixin/ModsScreenMixin.java
+++ src/main/java/com/github/goatgitter/modmanager/mixin/ModsScreenMixin.java
@@ -1,5 +1,11 @@
 package com.github.goatgitter.modmanager.mixin;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import org.apache.commons.lang3.Validate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,11 +17,21 @@ import com.github.goatgitter.modmanager.util.ModConfig;
 import com.github.goatgitter.modmanager.util.StickyNote;
 import com.mojang.blaze3d.systems.RenderSystem;
 
+import io.github.prospector.modmenu.ModMenu;
 import io.github.prospector.modmenu.gui.ModListEntry;
+import io.github.prospector.modmenu.gui.ModListWidget;
 import io.github.prospector.modmenu.gui.ModMenuTexturedButtonWidget;
 import io.github.prospector.modmenu.gui.ModsScreen;
+import io.github.prospector.modmenu.gui.entries.ParentEntry;
+import io.github.prospector.modmenu.util.HardcodedUtil;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.metadata.ModMetadata;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.NativeImageBackedTexture;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -27,20 +43,87 @@ import net.minecraft.util.Identifier;
  * 
  */
 @Mixin(ModsScreen.class)
-public class ModsScreenInit extends Screen{
-	private static final Log LOG = new Log("ModsScreenInit");
+public class ModsScreenMixin extends Screen{
+	private static final Log LOG = new Log("ModsScreenMixin");
 	private int paneY = 48;
 	private int buttonHeight = 20;
 	private int buttonWidth = 32;
 	private int canvasHeight = 64;
 	private ModListEntry selected;
+	private ModListWidget modList;
 	private static final TranslatableText TEXT_UNLOAD_TOOLTIP = new TranslatableText(ModManager.MOD_ID + ".unload.tooltip");
 	private static final TranslatableText TEXT_REFRESH_TOOLTIP = new TranslatableText(ModManager.MOD_ID + ".refresh.tooltip");
 	private static final Identifier UNLOAD_BTN_IMG = new Identifier(ModManager.MOD_ID, "button.png");
 	private static final Identifier REFRESH_BTN_IMG = new Identifier(ModManager.MOD_ID, "refresh.png");
-	public ModsScreenInit(Text title) {
+	public ModsScreenMixin(Text title) {
 		super(title);
 	}
+	
+	@Inject(at = @At("RETURN"), method = "render(Lnet/minecraft/client/util/math/MatrixStack;IIF)V")
+	public void drawIcon(CallbackInfo info) {	
+		LOG.enter("drawIcon");
+		if (selected instanceof ParentEntry)
+		{
+			ParentEntry entry = (ParentEntry) selected;
+			bindIconTexture(entry);
+		}
+		LOG.exit("drawIcon");
+	}
+
+	private void bindIconTexture(ParentEntry entry) {
+		ModMetadata metadata = entry.getMetadata();
+		Identifier iconLocation = new Identifier("modmenu", metadata.getId() + "_icon");
+		NativeImageBackedTexture icon = this.createIcon(entry);
+		if (icon != null) {
+			this.client.getTextureManager().registerTexture(iconLocation, icon);
+		} else {
+			iconLocation = ModListEntry.UNKNOWN_ICON;
+		}
+
+		this.client.getTextureManager().bindTexture(iconLocation);
+	}
+	
+	private NativeImageBackedTexture createIcon(ParentEntry entry) {
+		ModMetadata metadata = entry.getMetadata();
+		ModContainer container = null;
+		for(ModContainer mod:modList.getCurrentModSet())
+		{
+			if (mod.getMetadata().getId().equals(metadata.getId()))
+			{
+				container = mod;
+				break;
+			}
+		}
+		if (container != null)
+		{
+			try {
+				Path path = container.getPath(metadata.getIconPath(64 * MinecraftClient.getInstance().options.guiScale).orElse("assets/" + metadata.getId() + "/icon.png"));
+				
+				if (!Files.exists(path)) {
+					ModContainer modMenu = FabricLoader.getInstance().getModContainer(ModMenu.MOD_ID).orElseThrow(IllegalAccessError::new);
+					if (HardcodedUtil.modHasHardcodedIcon(metadata)) {
+						path = HardcodedUtil.getHardcodedIcon(metadata);
+					} else {
+						path = modMenu.getPath("assets/" + ModMenu.MOD_ID + "/grey_fabric_icon.png");
+					}
+				}
+				
+				try (InputStream inputStream = Files.newInputStream(path)) {
+					NativeImage image = NativeImage.read(Objects.requireNonNull(inputStream));
+					Validate.validState(image.getHeight() == image.getWidth(), "Must be square icon");
+					NativeImageBackedTexture tex = new NativeImageBackedTexture(image);
+					return tex;
+				}
+
+			} catch (Throwable t) {
+				LOG.error(String.format("Invalid icon for mod {}", metadata.getName()));
+				return null;
+			}
+		}
+		return null;
+		
+	}
+	
 	@Inject(at = @At("RETURN"), method = "init()V")
 	public void drawMenuButton(CallbackInfo info) {	
 		LOG.enter("drawMenuButton");
